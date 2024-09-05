358a5433cec8fcd5e04c13c16e5cecd57b16bde8
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-05 16:25:44 -0300

    Padrões de visibilidade

--- src/main/java/jackson/rocha/Principal.java
+++ src/main/java/jackson/rocha/Principal.java
@@ -1,14 +1,7 @@
 package jackson.rocha;
 
-import jackson.rocha.comercial.Pedido;
-import jackson.rocha.estoque.Produto;
-
 public class Principal {
     public static void main(String[] args) {
-        Pedido pedido = new Pedido();
-        Produto produto = new Produto();
-
-        System.out.println("Olá");
-
+      
     }
 }
--- src/main/java/jackson/rocha/estoque/Produto.java
+++ src/main/java/jackson/rocha/estoque/Produto.java
@@ -1,4 +1,36 @@
 package jackson.rocha.estoque;
 
+import java.util.Objects;
+import java.util.UUID;
+
 public class Produto {
+
+
+    private static final int QUANTIDADE_ESTOQUE_INICIAL = 100;
+
+    final String codigo;
+    public String nome;
+    int quantidadeEstoque;
+
+    Produto() {
+        this("Sem nome");
+    }
+
+    Produto(String nome) {
+        this(nome, QUANTIDADE_ESTOQUE_INICIAL);
+    }
+
+    Produto(String nome, int estoqueInicial) {
+        Objects.requireNonNull(nome, "Nome é obrigatório");
+
+        if (estoqueInicial < 0) {
+            throw new IllegalArgumentException("O estoque não pode ser negativo");
+        }
+
+        this.nome = nome;
+        this.quantidadeEstoque = estoqueInicial;
+        this.codigo = UUID.randomUUID().toString();
+    }
+
 }
+
