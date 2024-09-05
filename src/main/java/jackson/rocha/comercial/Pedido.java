5bab63dc8d30fc1165cc2ba78fbcef3c62c3ed2c
jacksonrocha <jackson_lbr@hotmail.com>
2024-09-04 22:26:38 -0300

    Separação por pacotes

--- src/main/java/jackson/rocha/comercial/Pedido.java
+++ src/main/java/jackson/rocha/comercial/Pedido.java
@@ -1,4 +1,11 @@
 package jackson.rocha.comercial;
 
+import jackson.rocha.estoque.Produto;
+
+import java.util.ArrayList;
+
 public class Pedido {
+
+    ArrayList<Produto> produtos;
+    Cliente cliente;
 }
